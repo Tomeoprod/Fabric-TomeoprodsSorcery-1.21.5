b37f6746cfe3084625134c0106d4408cdb83a93b
Tomeoprod <tmaupeu@gmail.com>
2025-05-13 22:08:44 +0200

    Initial commit

---
+++ src/main/java/net/tomeoprod/frigielssorcery/FrigielsSorceryClient.java
@@ -0,0 +1,10 @@
+package net.tomeoprod.frigielssorcery;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FrigielsSorceryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/tomeoprod/frigielssorcery/procedures/AbsorbeProcedure.java
@@ -0,0 +1,9 @@
+package net.tomeoprod.frigielssorcery.procedures;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+
+public class AbsorbeProcedure {
+    public void OnRightClick() {
+    }
+}
