d421c1e7ccff9d6fc30fd782914671887c3efc4b
Tomeoprod <tmaupeu@gmail.com>
2025-05-14 13:25:55 +0200

    t

--- src/main/java/net/tomeoprod/frigielssorcery/procedures/AbsorbeProcedure.java
+++
@@ -1,9 +0,0 @@
-package net.tomeoprod.frigielssorcery.procedures;
-
-import net.fabricmc.fabric.api.event.Event;
-import net.fabricmc.fabric.api.event.player.UseBlockCallback;
-
-public class AbsorbeProcedure {
-    public void OnRightClick() {
-    }
-}
